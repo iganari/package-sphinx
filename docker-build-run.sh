22f807dd0f77c3fb65f91635068f36e86b3da482
iganari <cigaguri@gmail.com>
2020-10-09 11:06:01 +0900

    modify run scrips

--- docker-build-run.sh
+++ docker-build-run.sh
@@ -12,11 +12,20 @@ set -eu
 
 docker build . -t ${_I_TAG}
 
-docker run --rm \
-    -it \
+# docker run --rm \
+#     -it \
+#     -v $BASEPATH:/opt/hejda/$DIREPATH \
+#     -w /opt/hejda/$DIREPATH \
+#     -h ${_I_TAG} \
+#     --name ${_I_TAG} \
+#     -p 8080:80 \
+#     ${_I_TAG} \
+#     /bin/ash
+
+docker run -d \
     -v $BASEPATH:/opt/hejda/$DIREPATH \
     -w /opt/hejda/$DIREPATH \
     -h ${_I_TAG} \
     --name ${_I_TAG} \
-    ${_I_TAG} \
-    /bin/ash
+    -p 8080:80 \
+    ${_I_TAG}
